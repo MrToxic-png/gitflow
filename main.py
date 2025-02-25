f5a1335e6b10b5cb4d180a58912484bbe7a4307e
Kids01 <gegeg@gmail.com>
2025-02-25 19:45:35 +0300

    GOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDA

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
-    print('GOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDAGOYDA')
+import os
+import shutil
+import datetime
+
+
+def make_reserve_arc(source, dest):
+    mamka = source.split('/')
+    shutil.copy(source, dest)
+    os.chdir(dest)
+    shutil.make_archive(datetime.datetime, "zip", root_dir='/'.join((dest, mamka)))
